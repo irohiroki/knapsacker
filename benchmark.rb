580c4a894b0eec7118e5cd326fc87bb802f38162
Hiroki Yoshioka <irohiroki@gmail.com>
2023-06-14 11:44:16 +0900

    Benchmark script

--- benchmark.rb
+++ benchmark.rb
@@ -11,6 +11,7 @@ end
 
 class Runner
   attr_reader :result
+  attr_accessor :capacity
 
   def initialize(input, capacity = 10)
     @input = input
@@ -24,7 +25,11 @@ class Runner
   end
 
   def load
-    @knapsacker = Knapsacker.new(items, capacity: @capacity)
+    @knapsacker = Knapsacker.new(@items, capacity: @capacity)
+  end
+
+  def load_items
+    @items = items
   end
 
   def run
@@ -32,9 +37,20 @@ class Runner
   end
 end
 
-r = Runner.new($stdin, ARGV[0])
-r.load
-Benchmark.bmbm do |x|
-  x.report { r.run }
+r = Runner.new($stdin)
+r.load_items
+(500..599).each do |i|
+  r.capacity = i
+  r.load
+  before = Time.now
+  r.run
+  time = Time.now - before
+  puts "#{i}\t#{time}"
 end
-puts "#{r.result.size} items: value #{r.result.sum(&:value)}, cost #{r.result.sum(&:cost)}" if ARGV[1] == '-v'
+
+# r = Runner.new($stdin, ARGV[0])
+# r.load
+# Benchmark.bm do |x|
+#   x.report { r.run }
+# end
+# puts "#{r.result.size} items: value #{r.result.sum(&:value)}, cost #{r.result.sum(&:cost)}" if ARGV[1] == '-v'
